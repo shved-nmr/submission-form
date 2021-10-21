1db538003976c4ced6a8c45cc34d8e225699c431
Erik Ruotsalainen <44022872+shved-nmr@users.noreply.github.com>
2021-10-21 14:43:33 +0300

    Fixed reading and parsing city data into array
    Removed index from JSON and rewrote the reading and parsing process

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 function InvalidMsgEmail(textbox) {
 
     var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
+    
     if (textbox.value === '') {
         textbox.setCustomValidity('Please fill in your email');
     } else if (textbox.value.match(regex)) {
@@ -18,16 +18,21 @@ function loadCities() {
 }
 
 function InvalidMsgCity(textbox) {
-    
-    $.ajax({
-        url: 'fi.json',
-        dataType: 'json',
-        success: function (data) {
-            $(data.cities).each(function (index, value) {
-                
-            });
-        }
+    $(document).ready(function () {
+        $.getJSON('fi.json', function (data) {
+                var arrItems = [];              // The array to store JSON data.
 
+                $.each(data, function (index, value) {
+                    arrItems.push(value.city);       // Push the value inside the array.
+                });
+
+                // Use the data in the array in whatever way you want it.
+                // I am using the array to populate a <select> dropdown list.
+
+            console.log(arrItems[0]);
+            console.log(arrItems[1]);
+            
+        });
     });
 
 
@@ -52,3 +57,19 @@ function InvalidMsgCity(textbox) {
 
 //    });
 //});
+
+//$.ajax({
+//    url: 'fi.json',
+//    dataType: 'json',
+//    success: function (data) {
+
+//        $(data.cities).each(function (index, value) {
+//            console.log('we made it here yeeeeea');
+//            arrItems.push(value.city);
+//        });
+
+
+//    }
+
+//});
+//console.log(arrItems[1]);
